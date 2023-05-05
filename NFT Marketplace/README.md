095f5812b928bd524f0dcd962e2508c8ef5fcc7b
Mithunsuresh <65971779+Mithun-Suresh@users.noreply.github.com>
2023-05-05 15:19:24 +0530

    Initial Commit

---
+++ NFT Marketplace/README.md
@@ -0,0 +1,9 @@
+Steps to deploy:
+
+git clone https://gitlabee.dt.renault.com/IRN-67134/atg-coe/blockchain/warranty_poc.git
+
+cd NFT Marketplace
+
+npm install
+
+npm start
---
+++ NFT Marketplace/src/pinata.js
@@ -0,0 +1,91 @@
+//require('dotenv').config();
+//const key = process.env.REACT_APP_PINATA_KEY;
+//const secret = process.env.REACT_APP_PINATA_SECRET;
+
+const axios = require('axios');
+const FormData = require('form-data');
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: "f77930d897f012402330",
+                pinata_secret_api_key: "56d0c03492d8f134115d93b0eed06c248e826eeacbdeb79b4df60c5f5c1b8928",
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadFileToIPFS = async(file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: "f77930d897f012402330",
+                pinata_secret_api_key: "56d0c03492d8f134115d93b0eed06c248e826eeacbdeb79b4df60c5f5c1b8928",
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
